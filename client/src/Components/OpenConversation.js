23456c71685f2ccc5178182cfdde5b48e3f87b9c
Adam Birgenheier <adamcarlbirgenheier@gmail.com>
2020-10-20 09:29:22 -0700

    more backend functionality update

--- client/src/Components/OpenConversation.js
+++ client/src/Components/OpenConversation.js
@@ -1,7 +1,43 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Button, Form, InputGroup } from "react-bootstrap";
+import {
+  useConversations,
+  selectedConversation,
+} from "../Context/ConversationsProvider";
 function OpenConversation() {
-  return <div></div>;
+  const [text, setText] = useState("");
+  const { sendMessage } = useConversations();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    sendMessage(
+      selectedConversation.recipients.map(({ id }) => {
+        id;
+      }),
+      text
+    );
+  };
+  return (
+    <div className="d-flex flex-column flex-grow-1">
+      <div className="flex-grow-1 overflow-auto"></div>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="m-2">
+          <InputGroup>
+            <Form.Control
+              as="textarea"
+              required="true"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+              style={{ height: "75px", resize: "none" }}
+            >
+              <InputGroup.Append>
+                <Button type="submit"></Button>
+              </InputGroup.Append>
+            </Form.Control>
+          </InputGroup>
+        </Form.Group>
+      </Form>
+    </div>
+  );
 }
 
 export default OpenConversation;
--- client/src/Context/ConversationsProvider.js
+++ client/src/Context/ConversationsProvider.js
@@ -22,13 +22,47 @@ export function ConversationsProvider({ children }) {
     });
   };
 
+  function addMessageToConversation({ recipients, text, sender }) {
+    setConversations((prevConversations) => {
+      let madeChange = false;
+      const newMessage = { sender, text };
+      const newConversations = prevConversations.map((conversation) => {
+        if (arrayEquality(conversation.recipients, recipients)) {
+          madeChange = true;
+          return {
+            ...conversation,
+            messages: [conversation.messages, newMessage],
+          };
+        }
+        return conversation;
+      });
+      if (madeChange) {
+        return newConversations;
+      } else {
+        return [...prevConversations, { recipients, messages: [nweMessage] }];
+      }
+    });
+  }
+
+  function sendMessage(recipients, text) {
+    addMessageToConversation({ recipients, text, sender: id });
+  }
+
   const formattedConversations = conversations.map((conversations, index) => {
     const recipients = conversations.recipients.map((recipient) => {
       const contact = contacts.find((contact) => {
         return contact.id === recipient;
       });
-      const name = (contact && contact.name) || recipient;
+      const name = (contact && contact.name) || message.sender;
       return { id: recipient, name };
+    });
+    const messages = conversation.messages.map((message) => {
+      const contact = contacts.find((contact) => {
+        return contact.id === message.sender;
+      });
+      const name = (contact && contact.name) || message.sender;
+      const fromMe = id === message.sender;
+      return { ...message, messages, senderName: nme, fromMe };
     });
     const selected = index === selectedConversationIndex;
     return { ...conversation, recipients, selected };
@@ -37,10 +71,22 @@ export function ConversationsProvider({ children }) {
     conversations: formattedConversations[selectedConversationIndex],
     selectedConversationIndex: setSelectedConversationIndex,
     createConversations,
+    sendMessage,
   };
   return (
     <ConversationsProvider.Provider value={value}>
       {children}
     </ConversationsProvider.Provider>
   );
+}
+
+function arrayEquality(a, b) {
+  if (a.length !== b.length) {
+    return false;
+  }
+  a.sort();
+  b.sort();
+  return a.every((element, index) => {
+    return element === b[index];
+  });
 }
